4923f06b9c880abf53a9b9a15fb9a1b3c5893483
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-14 13:03:40 +0900

    メイン画面に名前が表示されるようにした

--- app/src/main/java/com/ntetz/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/aboutme/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.ntetz.android.aboutme
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
