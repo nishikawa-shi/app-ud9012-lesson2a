23069f5d1990ae65a8e31ec2ad854b5e8b0d784b
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-14 22:20:48 +0900

    findViewByIdを使わずに済むようにした

--- app/src/main/java/com/ntetz/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/aboutme/MainActivity.kt
@@ -1,32 +1,34 @@
 package com.ntetz.android.aboutme
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.ntetz.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             this.addNickName(it)
         }
     }
 
     private fun addNickName(view: View) {
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         //キーボードを隠す
         val imn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
